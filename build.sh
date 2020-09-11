336a6ed601a504f36af8fbc125529a37b035702f
bcis93 <bryancisneros93@gmail.com>
2020-09-11 14:00:26 -0700

    update build script

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
-git submodule update --init
+git submodule update --init --recursive
 
-MACHINE=raspberrypi0 GPU_MEM=16 source layers/poky/oe-init-build-env
+source layers/poky/oe-init-build-env
+
+if grep -q "MACHINE=\"raspberrypi0\"" ./conf/local.conf; then
+    echo -n ""
+else
+    echo "MACHINE=\"raspberrypi0\"" >> ./conf/local.conf
+fi
+
+if grep -q "GPU_MEM=\"16\"" ./conf/local.conf; then
+    echo -n ""
+else
+    echo "GPU_MEM=\"16\"" >> ./conf/local.conf
+fi
 
 bitbake-layers show-layers | grep "meta-raspberrypi" > /dev/null
 layer_info=$?
@@ -11,6 +23,17 @@ else
 	echo "meta-raspberrypi layer already exists"
 fi
 
+bitbake-layers show-layers | grep "meta-oe" > /dev/null
+layer_info=$?
+if [ $layer_info -ne 0 ];then
+	echo "Adding meta-oe layer"
+	bitbake-layers add-layer ../layers/meta-openembedded/meta-oe
+else
+	echo "meta-oe layer already exists"
+fi
+
+bitbake-layers show-layers | grep "meta-cizies-looper" > /dev/null
+layer_info=$?
 if [ $layer_info -ne 0 ];then
 	echo "Adding meta-cizies-looper layer"
 	bitbake-layers add-layer ../layers/meta-cizies-looper
